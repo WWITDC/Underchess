80da5fdb858377641d6b81add1c329985894fee1
ApolloZhu <zhuzhiyu20010417@outlook.com>
2016-02-13 15:05:30 -0500

    Recovered from Original Repo
    恢复为了转移所有权之前的样子
    This should be the same as this repo in another account

---
+++ underchess/UCConfigManager.swift
@@ -0,0 +1,17 @@
+//
+//  UCConfigManager.swift
+//  Underchess
+//
+//  Created by Apollonian on 16/2/13.
+//  Copyright © 2016年 WWITDC. All rights reserved.
+//
+
+import UIKit
+
+func NSUSD() -> NSUserDefaults {
+    return NSUserDefaults.standardUserDefaults()
+}
+
+class UCConfigManager {
+    
+}
