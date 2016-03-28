ece8e17970c9756a633736bf97f7f62a10573221
ApolloZhu <zhuzhiyu20010417@outlook.com>
2016-03-27 23:10:41 -0400

    First completed version. 2333
    Ready to publish

--- underchess/UCArenaViewController.swift
+++ underchess/UCArenaViewController.swift
@@ -10,7 +10,29 @@
 
 import UIKit
 
-class UCArenaViewController: UIViewController, UCPieceProvider {
+enum UserInputError:ErrorType{
+    case ControlUnownedPiece
+    case PlaceOnAnoherPiece
+    case CrossNoncrossbleObject
+}
+
+enum UCGamePhase{
+    case Ended, Playing, NotStarted
+}
+
+extension UserInputError: CustomStringConvertible{
+    var description:String{
+        get{
+            switch self{
+            case .CrossNoncrossbleObject: return "Cross noncrossble object"
+            case .ControlUnownedPiece: return "Control unowned piece"
+            case .PlaceOnAnoherPiece: return "Place on another piece"
+            }
+        }
+    }
+}
+
+class UCArenaViewController: UIViewController, UCPieceProvider, UCPieceViewDelegate {
     
     static let sharedInstance = UCArenaViewController()
     var needAnimation = true
@@ -18,22 +40,20 @@ class UCArenaViewController: UIViewController, UCPieceProvider {
     var piecesWithStyle : [UCPieceView]?
     
     func pieces() -> [UCPieceView]? {
-        piecesWithStyle = [UCPieceView]()
-        piecesWithStyle?.append(UCPieceView(color: .ucPieceRedColor()))
-        piecesWithStyle?.append(UCPieceView(color: .ucPieceRedColor()))
-        piecesWithStyle?.append(UCPieceView(color: .whiteColor(), strokeColor: .blackColor(), strokeWdith: 1))
-        piecesWithStyle?.append(UCPieceView(color: .ucPieceGreenColor()))
-        piecesWithStyle?.append(UCPieceView(color: .ucPieceGreenColor()))
+        // MARK: Maybe Override Style with Key: preferedStyle
         return piecesWithStyle
     }
     
-//    var recordIdentifier: String?
+    //    var recordIdentifier: String?
     
     var arenaView: UCArenaView?
     
-    private var situation = [0,0,2,1,1]
+    // false -> 0; true -> 1
+    private var situation : [Bool?] = [false,false,nil,true,true]
     private var currentPlayer = false
-    private var didStart = false
+    var didStartMoving = UITouchPhase.Ended
+    var didStartGame = UCGamePhase.NotStarted
+    var touchedPiece : Int?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,15 +61,17 @@ class UCArenaViewController: UIViewController, UCPieceProvider {
         NC.addObserver(self, selector: #selector(orientatoinDidChange), name: UIDeviceOrientationDidChangeNotification, object: nil)
         arenaView = UCArenaView(father: view)
         arenaView?.provider = self
+        arenaView?.pieceViewDelegate = self
         view.addSubview(arenaView!)
         /*
-        if let identifier = recordIdentifier {
-            let data = loadGameRecord(identifier)
-            situation = data["situation"] as! [Int]
-            currentPlayer = data["curPlayer"] as! Bool
-            didStart = data["started"] as! Bool
-            // Consider later
-        } else {*/
+         if let identifier = recordIdentifier {
+         let data = loadGameRecord(identifier)
+         situation = data["situation"] as! [Int]
+         currentPlayer = data["currentPlayer"] as! Bool
+         didStartMoving = data["started"] as! Bool
+         didStartGame = data["didStartGame"] as UITouchPhase
+         // Consider later
+         } else {*/
         /*}*/
     }
     
@@ -68,35 +90,133 @@ class UCArenaViewController: UIViewController, UCPieceProvider {
         arenaView?.setupAgain(frame: view.frame)
     }
     
+    func touchUpInside(tag: Int) throws {
+        switch didStartMoving {
+        case .Began:
+            if situation[tag] != nil {
+                touchedPiece = nil
+                didStartMoving = .Ended
+                throw UserInputError.PlaceOnAnoherPiece
+            } else if touchedPiece! + tag == 7 || abs(touchedPiece! - tag) == 3{
+                touchedPiece = nil
+                didStartMoving = .Ended
+                throw UserInputError.CrossNoncrossbleObject
+            } else {
+                arenaView?.movePiece(pieceTag: touchedPiece!, toPlace: tag)
+                swap(&situation[touchedPiece!], &situation[tag])
+                touchedPiece = nil
+                didStartMoving = .Ended
+                var arg1 = 0, arg2 = 0
+                for i in 0...4{
+                    if situation[i] == !currentPlayer{
+                        arg1 += i
+                    } else if situation[i] != nil{
+                        arg2 += i
+                    }
+                }
+                if arg1 == 4 && (arg2 == 2 || arg2 == 3){
+                    // TODO: Do something useful after one player won
+                    let dialog = LLDialog()
+                    
+                    // Set title.
+                    currentPlayer ? (dialog.title = "Player Green Won") : (dialog.title = "Player Red Won")
+                    
+                    // Set content.
+                    dialog.content = "Congratulations. Do you want to play again? \n P.S. If you choose no, you can always shake your device and play again."
+                    
+                    // Set the buttons
+                    dialog.setYesButton(self, title: "YES", action: "next")
+                    dialog.setNoButton(self, title: "NO", action: nil)
+                    
+                    // Don't forget this line.
+                    dialog.refreshUI()
+                    
+                    // At last, add it to your view.
+                    self.view.addSubview(dialog)
+                    didStartGame = .Ended
+                }
+                currentPlayer = !currentPlayer
+            }
+        case .Ended:
+            if didStartGame == .NotStarted{
+                didStartGame = .Playing
+            }
+            if situation[tag] == currentPlayer{
+                touchedPiece = tag
+                didStartMoving = .Began
+            } else {
+                throw UserInputError.ControlUnownedPiece
+            }
+        default:
+            break
+        }
+    }
     
+    func getError(error: ErrorType) {
+        let dialog = LLDialog()
+        
+        // Set title.
+        dialog.title = "Permission denied"
+        
+        // Set content.
+        switch error{
+        case UserInputError.ControlUnownedPiece:
+            dialog.content = "You don't own that piece"
+        case UserInputError.PlaceOnAnoherPiece:
+            dialog.content = "You can't place your piece on another one"
+        case UserInputError.CrossNoncrossbleObject:
+            dialog.content = "That movement is not appropriate"
+        default: dialog.content = "Some error occured"
+        }
+
+        // Set the buttons
+        dialog.setYesButton(self, title: "OK", action: nil)
+        dialog.setNoButton(self, title: "", action: nil)
+        // Don't forget this line.
+        dialog.refreshUI()
+        
+        // At last, add it to your view.
+        view.addSubview(dialog)
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        super.motionEnded(motion, withEvent: event)
+        if motion == UIEventSubtype.MotionShake{
+            switch didStartGame {
+            case .NotStarted:
+                currentPlayer = !currentPlayer
+            case .Ended:
+                next()
+            default:
+                break
+            }
+        }
+    }
+    
+    func next(){
+        presentViewController(UCArenaViewController(), animated: true, completion: nil)
+    }
     
     /*
-    private func loadGameRecord(record: String) -> [String:AnyObject]{
-        /*
-         Game Record On Start Up (Does not support history):
-         [
-         "Situation":[0,0,2,1,1],
-         "curPlayer":false,
-         "started":false
-         ]
-         */
-        let data = NSKeyedUnarchiver.unarchiveObjectWithData(NSUD.objectForKey(record) as! NSData)
-        return data as! [String:AnyObject]
-    }
-    
-    private func saveGameRecord(record: String){
-        dispatch_async(dispatch_get_main_queue(), {
-            let data = NSKeyedArchiver.archivedDataWithRootObject(["situation":self.situation, "curPlayer":self.currentPlayer, "started":self.didStart])
-            NSUD.setObject(data, forKey: record)
-            NSUD.synchronize()
-        })
-    }*/
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
-    
+     private func loadGameRecord(record: String) -> [String:AnyObject]{
+     /*
+     Game Record On Start Up (Does not support history):
+     [
+     "situation":[0,0,2,1,1],
+     "currentPlayer":false,
+     "didStartMoving":.Ended,
+     "didStartGame": false
+     ]
+     */
+     let data = NSKeyedUnarchiver.unarchiveObjectWithData(NSUD.objectForKey(record) as! NSData)
+     return data as! [String:AnyObject]
+     }
+     
+     private func saveGameRecord(record: String){
+     dispatch_async(dispatch_get_main_queue(), {
+     let data = NSKeyedArchiver.archivedDataWithRootObject(["situation":self.situation, "currentPlayer":self.currentPlayer, "didStartMoving":self.didStartMoving,"didStartGame":self.didStartGame])
+     NSUD.setObject(data, forKey: record)
+     NSUD.synchronize()
+     })
+     }*/
 }
--- underchess/UCCustomizePieceViewController.swift
+++ underchess/UCCustomizePieceViewController.swift
@@ -17,20 +17,4 @@ class UCCustomizePieceViewController: UIViewController {
         presentViewController(UCArenaViewController.sharedInstance, animated: true, completion: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
